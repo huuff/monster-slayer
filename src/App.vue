581e30ca1a3d512be9adb91c97a10596efe2a01d
Haf <haf@protonmail.ch>
2021-11-11 00:42:58 +0100

    getting started

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="row">
+    <div class="col-lg-6 offset-lg-3 d-flex flex-column">
+      <h1 class="text-center mb-4">Monster Slayer</h1>
+      <div class="mb-3">
+        <h2>Monster</h2>
+        <div class="progress">
+          <div class="progress-bar bg-danger" :style="{width: monsterHealth + '%'}"></div>
+        </div>
+      </div>
+      <div class="mb-5">
+        <h2>Player</h2>
+        <div class="progress">
+          <div class="progress-bar" :style="{width: playerHealth + '%'}"></div>
+        </div>
+      </div>
+      <div class="d-flex flex-column align-items-center">
+        <button class="btn btn-primary btn-lg" @click="attackMonster">ATTACK</button>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    }
+  },
+  methods: {
+    attackMonster() {
+      this.monsterHealth -= 10;
+    },
+  },
+
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
