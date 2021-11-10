ee52bdf9d470a195eaa9550e95444f1b9e5c1b04
Haf <haf@protonmail.ch>
2021-11-11 00:22:23 +0100

    fix flake; add bootstrap

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   outputs = { self, nixpkgs, ...}:
   let
-    system = "x86_84-linux";
+    system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in with pkgs; {
     devShell.${system} = mkShell {
